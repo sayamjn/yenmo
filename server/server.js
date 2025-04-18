65b4b40e7a0fbee33ea21f4ad128467210fb0a86
Sayam Jain <sayamjain0708@gmail.com>
2025-04-18 11:23:19 +0530

    backend init

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
